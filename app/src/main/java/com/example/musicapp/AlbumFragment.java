49092770406edc8036c9dca2e7ff9a8442da2bc9
prince91221 <prince.sharma930@gmail.com>
2021-02-25 03:33:42 +0530

    Added Album Item List

--- app/src/main/java/com/example/musicapp/AlbumFragment.java
+++ app/src/main/java/com/example/musicapp/AlbumFragment.java
@@ -4,11 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
 public class AlbumFragment extends Fragment {
+
+    ListView listView;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        listView = (ListView) getView().findViewById(R.id.album_list);
+
+        ArrayList<String> albumList = new ArrayList<String>();
+        albumList.add("Rockstar");
+        albumList.add("Bohemian Rhapsody");
+        albumList.add("Tamasha");
+        albumList.add("Altaf Raja");
+        albumList.add("Kailash Kher");
+        albumList.add("AR Rehman");
+        albumList.add("Sunidha Chauhan");
+        albumList.add("Sunidha Chauhan 1");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(view.getContext(), android.R.layout.simple_list_item_1, albumList);
+        listView.setAdapter(arrayAdapter);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
