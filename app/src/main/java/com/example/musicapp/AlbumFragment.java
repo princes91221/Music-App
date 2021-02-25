07854766c90ade8dda78aa8fb885d81813e965d5
prince91221 <prince.sharma930@gmail.com>
2021-02-25 14:51:45 +0530

    Added songs to Album row and showing dialog

--- app/src/main/java/com/example/musicapp/AlbumFragment.java
+++ app/src/main/java/com/example/musicapp/AlbumFragment.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -30,17 +34,17 @@ public class AlbumFragment extends Fragment {
 
         // Create Data
         ArrayList<AlbumObject> albumList = new ArrayList<AlbumObject>();
-        albumList.add(new AlbumObject(R.drawable.bohemian, "Bohemian Rhapsody", "Produced by Queen"));
-        albumList.add(new AlbumObject(R.drawable.hybrid, "Hybrid Theory", "Produced by Linkin Park"));
-        albumList.add(new AlbumObject(R.drawable.kohinoor, "Kohinoor", "Produced by Divine"));
-        albumList.add(new AlbumObject(R.drawable.maroon5, "Live from Le Cabaret", "Produced by Maroon 5"));
-        albumList.add(new AlbumObject(R.drawable.mtbmb, "Music To Be Murdered By", "Produced by Eminem"));
-        albumList.add(new AlbumObject(R.drawable.rated_r, "Rated R", "Produced by Rihanna"));
-        albumList.add(new AlbumObject(R.drawable.reckless, "Reckless", "Produced by Bryan Adams"));
-        albumList.add(new AlbumObject(R.drawable.recovery, "Recovery", "Produced by Eminem"));
-        albumList.add(new AlbumObject(R.drawable.reputation, "Reputation", "Produced by Taylor Swift"));
-        albumList.add(new AlbumObject(R.drawable.rockstar, "Rockstar", "Produced by AR Rehman"));
-        albumList.add(new AlbumObject(R.drawable.tamasha, "Tamasha", "Produced by AR Rehman"));
+        albumList.add(new AlbumObject(R.drawable.bohemian, "Bohemian Rhapsody", "Produced by Queen", 5));
+        albumList.add(new AlbumObject(R.drawable.hybrid, "Hybrid Theory", "Produced by Linkin Park", 8));
+        albumList.add(new AlbumObject(R.drawable.kohinoor, "Kohinoor", "Produced by Divine", 7));
+        albumList.add(new AlbumObject(R.drawable.maroon5, "Live from Le Cabaret", "Produced by Maroon 5", 3));
+        albumList.add(new AlbumObject(R.drawable.mtbmb, "Music To Be Murdered By", "Produced by Eminem", 5));
+        albumList.add(new AlbumObject(R.drawable.rated_r, "Rated R", "Produced by Rihanna", 7));
+        albumList.add(new AlbumObject(R.drawable.reckless, "Reckless", "Produced by Bryan Adams", 1));
+        albumList.add(new AlbumObject(R.drawable.recovery, "Recovery", "Produced by Eminem", 9));
+        albumList.add(new AlbumObject(R.drawable.reputation, "Reputation", "Produced by Taylor Swift", 8));
+        albumList.add(new AlbumObject(R.drawable.rockstar, "Rockstar", "Produced by AR Rehman", 2));
+        albumList.add(new AlbumObject(R.drawable.tamasha, "Tamasha", "Produced by AR Rehman", 4));
 
         // Make Custom Adapter
         AlbumAdapter albumAdapter = new AlbumAdapter(view.getContext(), R.layout.album_row, albumList);
@@ -76,11 +80,25 @@ public class AlbumFragment extends Fragment {
 
             TextView albumDescription = convertView.findViewById(R.id.album_description);
 
-            imageView.setImageResource(getItem(position).getImage());
+            TextView numberOfSongs = convertView.findViewById(R.id.songs_number);
 
-            albumName.setText(getItem(position).getName());
+            AlbumObject albumObject = getItem(position);
+            imageView.setImageResource(albumObject.getImage());
 
-            albumDescription.setText(getItem(position).getDescription());
+            albumName.setText(albumObject.getName());
+
+            albumDescription.setText(albumObject.getDescription());
+
+            numberOfSongs.setText(String.valueOf(albumObject.getNumberOfSongs()) + " Songs");
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO: Show the detail page of album, sorry due to lack of time I was not able to show details and songs of album
+                    String title = "Show album details: " + albumObject.getName();
+                    new AlertDialog.Builder(v.getContext()).setTitle(title).show();
+                }
+            });
 
             return convertView;
         }
--- app/src/main/java/com/example/musicapp/AlbumObject.java
+++ app/src/main/java/com/example/musicapp/AlbumObject.java
@@ -4,11 +4,13 @@ public class AlbumObject {
     int Image;
     String Name;
     String Description;
+    int NumberOfSongs;
 
-    public AlbumObject(int image, String name, String description) {
+    public AlbumObject(int image, String name, String description, int numberOfSongs) {
         Image = image;
         Name = name;
         Description = description;
+        NumberOfSongs = numberOfSongs;
     }
 
     public int getImage() {
@@ -29,6 +31,14 @@ public class AlbumObject {
 
     public String getDescription() {
         return Description;
+    }
+
+    public int getNumberOfSongs() {
+        return NumberOfSongs;
+    }
+
+    public void setNumberOfSongs(int numberOfSongs) {
+        NumberOfSongs = numberOfSongs;
     }
 
     public void setDescription(String description) {
